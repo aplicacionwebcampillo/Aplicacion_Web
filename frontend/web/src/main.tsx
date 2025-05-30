0537b08a25de81d921c89d7eec6732ad122bc595
aplicacionwebcampillo <aplicacionwebcampillo@gmail.com>
2025-05-30 08:48:23 +0200

    Configuracion local-funcionando

--- frontend/web/src/main.tsx
+++ frontend/web/src/main.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { CarritoProvider } from "./context/CarritoContext";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-   <HashRouter>
+   <BrowserRouter>
     <CarritoProvider>
       <App />
     </CarritoProvider>
-   </HashRouter>
+   </BrowserRouter>
   </React.StrictMode>,
 );
 
