2e4991b400ada790c3d582361e12f2ba0a0469b8
aplicacionwebcampillo <aplicacionwebcampillo@gmail.com>
2025-06-30 10:32:52 +0200

    Update UltimasNoticias.tsx

--- frontend/web/src/components/UltimasNoticias.tsx
+++ frontend/web/src/components/UltimasNoticias.tsx
@@ -50,7 +50,9 @@ export default function UltimasNoticias() {
           className="h-[15rem] w-auto object-cover"
         />
       </div>
-      <h3 className="text-lg font-semibold mb-1 text-negro">{noticia.titular}</h3>
+      <div className="p-4 text-center w-full">
+        <h3 className="text-lg font-semibold mb-1 text-negro">{noticia.titular}</h3>
+      </div>
     </Link>
   ))}
 </div>
