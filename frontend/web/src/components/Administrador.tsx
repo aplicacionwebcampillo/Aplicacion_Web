c3def36f6707fbb01bf13b7947be47a83417155c
aplicacionwebcampillo <aplicacionwebcampillo@gmail.com>
2025-08-16 18:21:26 +0200

    Actualizo socios

--- frontend/web/src/components/Administrador.tsx
+++ frontend/web/src/components/Administrador.tsx
@@ -235,7 +235,7 @@ export default function Administrador() {
       ? "email"
       : "text"
   }
-  placeholder={campo}
+  placeholder={"Fecha de nacimiento dd/mm/aaaa"}
   className="rounded-[1rem] font-poetsen w-full border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-cyan-500"
   value={(admin as any)?.[campo] || ""}
   onFocus={(e) => {
--- frontend/web/src/components/Socios.tsx
+++ frontend/web/src/components/Socios.tsx
@@ -315,17 +315,30 @@ export default function Socios() {
             </>
           ) : (
             <input
-              type={campo === "fecha_nacimiento" ? "date" : "text"}
-              placeholder={campo}
-              className="rounded-[1rem] font-poetsen w-full border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-cyan-500"
-              value={(socio as any)?.[campo] || ""}
-              onChange={(e) =>
-                setSocio(prev => ({
-                  ...prev!,
-                  [campo]: e.target.value || "",
-                }))
-              }
-            />
+  type={
+    campo === "fecha_nacimiento"
+      ? (socio as any)?.[campo] ? "date" : "text"
+      : "text"
+  }
+  placeholder={"Fecha de nacimiento dd/mm/aaaa"}
+  className="rounded-[1rem] font-poetsen w-full border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-cyan-500"
+  value={(socio as any)?.[campo] || ""}
+  onFocus={(e) => {
+    if (campo === "fecha_nacimiento") e.target.type = "date";
+  }}
+  onBlur={(e) => {
+    if (campo === "fecha_nacimiento" && !e.target.value) {
+      e.target.type = "text";
+    }
+  }}
+  onChange={(e) =>
+    setSocio((prev) => ({
+      ...prev!,
+      [campo]: e.target.value || "",
+    }))
+  }
+/>
+
           )}
         </div>
       )
